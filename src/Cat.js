491df2a54fed4827792ae7f9d8016269d08838d0
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-05-12 14:56:07 -0400

    add styling to button

--- src/Cat.js
+++ src/Cat.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Cat = ({ image, handleClick }) => {
   return (
+    <>
     <div>
-      <button onClick={handleClick}>Randomize</button>
+      <button className='btn' onClick={handleClick}>Randomize</button>
+    </div>
+    <div>
       <img height={'50%'} width={'50%'} src={image} alt="" />
     </div>
-  
+    </>
   )
 }
 export default Cat;
