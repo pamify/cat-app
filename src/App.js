7448e3b012416454101252280d6a5ea52caa6252
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-05-12 13:37:29 -0400

    write function to fetch cat data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
-  );
-}
+
+  useEffect(() => {
+    getCats();
+  }, []);
+
+  const getCats = async () => {
+    const response = await fetch('https://api.thecatapi.com/v1/images/search');
+    const data = await response.json();
+    console.log('data-->', data);
+  }
+
+
+	return (
+		<div className="App">
+			<h1>Hello React!</h1>
+		</div>
+	);
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -9,3 +9,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
+
+// ReactDOM.render(<App />, document.getElementById('root'));
